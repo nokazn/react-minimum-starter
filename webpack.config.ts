260b620bc8c18c23ba3bd9a184fb4e21d607d038
nokazn <nokazn@gmail.com>
2020-10-05 11:45:23 +0900

    chore: add comments to webpack.config

--- webpack.config.ts
+++ webpack.config.ts
@@ -6,8 +6,10 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const config: Configuration = {
   mode: isProduction ? 'production' : 'development',
+  // https://github.com/gaearon/react-hot-loader#getting-started
   entry: ['react-hot-loader/patch', './src/index.tsx'],
   output: {
+    // 絶対パス
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
@@ -15,6 +17,7 @@ const config: Configuration = {
     rules: [
       {
         test: /\.scss$/,
+        // sass-loader -> css-loader -> style-loader の順に実行
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
@@ -27,6 +30,7 @@ const config: Configuration = {
     ],
   },
   plugins: [
+    // html ファイルに script タグを挿入
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
@@ -34,6 +38,8 @@ const config: Configuration = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     alias: {
+      // react-dom のかわりに @hot-loader/react-dom を使用する
+      // https://github.com/gaearon/react-hot-loader#hot-loaderreact-dom
       'react-dom': '@hot-loader/react-dom',
     },
   },
